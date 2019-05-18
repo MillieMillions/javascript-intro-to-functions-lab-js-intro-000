1c9a7ad1c8c7d0285344795eeb84f222c7d3fb19
Millie <milliealexblack@gmail.com>
2019-05-18 04:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function sayHiToGrandma(string) {
   // if uppercase, return "Yes indeed" or If lowercase, return "I can't hear you!"
   if (string === string.toUpperCase()) return "YES INDEED!"
   if (string === string.toLowerCase()) return "I can't hear you!"
-  if (string === "I love you, too.") return "I love you, Grandma."
+  if (string === "I love you, Grandma.") return "I love you, too."
 }
